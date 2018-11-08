e464eca9f804a1778b51bad13292dd54c7dac5f9
Benjamin Humphrey <benjamin@dovetailapp.com>
2018-11-08 15:24:49 +1100

    fix: Story

--- src/components/site/StaffMemberCard/__stories__/index.stories.tsx
+++ src/components/site/StaffMemberCard/__stories__/index.stories.tsx
@@ -1,12 +1,12 @@
-import * as benjamin from "@heydovetail/website/pages/company/about/benjamin.jpg";
 import { storiesOf } from "@heydovetail/website/util/stories";
 import React from "react";
 import { StaffMemberCard } from "../";
+import avatar from "./avatar.jpg";
 
 storiesOf("Site/StaffMemberCard", module).add("default", () => (
   <StaffMemberCard
     bio="Benjamin is a kiwi designer, developer, and entrepreneur living in Sydney. He loves creating world-class products and believes in the power of user research to do so. Previously Benjamin has led design projects at Atlassian, Delicious, and Ubuntu."
-    image={benjamin}
+    image={avatar}
     name="Benjamin Humphrey"
     role="CEO &amp; Co-founder"
   />
